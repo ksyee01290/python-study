d42cec1f9c7fcbab89fd9dde15cdb61a4ca91531
sourcejung <sourcejung@gmail.com>
2026-04-15 19:48:01 +0900

    20일차 개선

--- python/day20_1.py
+++ python/day20_1.py
@@ -1,24 +1,25 @@
 import os,shutil,datetime
+
 lis = os.listdir(".")
 
 os.makedirs("python", exist_ok=True)
 os.makedirs("text", exist_ok=True)
 os.makedirs("csv", exist_ok=True)
+
 for i in lis:
+    folder = ""
+    
     name, ext = os.path.splitext(i)
     if ext == ".py":
-        shutil.move(i,"python")
-        
-        with open("text/log.txt", "a", encoding="utf-8") as f:
-            f.write(f"{datetime.datetime.now()} - {i} → {'python'} 으로 이동\n")
+        folder = "python"
         
     elif ext == ".txt":
-        shutil.move(i,"text")
-        with open("text/log.txt", "a", encoding="utf-8") as f:
-            f.write(f"{datetime.datetime.now()} - {i} → {'text'} 으로 이동\n")
-            
+        folder = "text"
+        
     elif ext == ".csv":
-        shutil.move(i,"csv")
+        folder = "csv"
+        
+    if folder:
+        shutil.move(i,folder)
         with open("text/log.txt", "a", encoding="utf-8") as f:
-            f.write(f"{datetime.datetime.now()} - {i} → {'csv'} 으로 이동\n")
-            
+            f.write(f"{datetime.datetime.now()} - {i} → {folder} 으로 이동\n")           
