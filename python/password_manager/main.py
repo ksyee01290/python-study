8e3e2c200f95b7a328bff0739af77253be6fba4c
sourcejung <sourcejung@gmail.com>
2026-04-18 04:34:41 +0900

    21일차 비밀번호 관리자 리팩토링

--- python/password_manager/main.py
+++ python/password_manager/main.py
@@ -1,9 +1,33 @@
+def save_password():
+    site = input("사이트명 입력:")
+    password = input("비밀번호 입력:")
+    passwords[site] = password
+    print(f"{site}에 저장되었습니다.")
+
+def search_password():
+    site = input("사이트명 입력:")
+    print(passwords[site])
+
+def delete_password():
+    site = input("사이트명 입력:")
+    del passwords[site]
+
+def exit_program():
+    with open("text/passwords.txt","w", encoding="utf-8") as f:
+        for key, value in passwords.items():
+            f.write(f"{key},{value}\n")   
+
+def load_passwords():
+    try:
+        with open("text/passwords.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                data = line.strip().split(",")
+                passwords[data[0]] = data[1]
+    except Exception as e:
+        print("에러발생:", e)
 
 passwords = {}
-with open("text/passwords.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        data = line.strip().split(",")
-        passwords[data[0]] = data[1]
+load_passwords()
 
 while True:
     print("메뉴 출력")
@@ -15,22 +39,11 @@ while True:
     num = int(input("메뉴 입력:"))
 
     if num == 1:
-        sight = input("사이트명 입력:")
-        password = input("비밀번호 입력:")
-        passwords[sight] = password
-        print(passwords[sight]+"에 저장되었습니다.")
-
+        save_password()
     elif num == 2:
-        sight = input("사이트명 입력:")
-        print(passwords[sight])
-
-
+        search_password()
     elif num == 3:
-        key = input("사이트명 입력:")
-        del passwords[key]
-                
+        delete_password()             
     elif num == 4:
-        with open("text/passwords.txt","w", encoding="utf-8") as f:
-            for key, value in passwords.items():
-                f.write(f"{key},{value}\n")      
+        exit_program()
         break
