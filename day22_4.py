0b146a932bb04c92717d6b43c472ddaf924d08d7
sourcejung <sourcejung@gmail.com>
2026-04-20 15:14:18 +0900

    22일차 3가지 예제

--- day22_4.py
+++ day22_4.py
@@ -1,4 +1,5 @@
 class Menu:
+    """카페 메뉴 할인"""
     def __init__(self, name, price):
         self.name = name
         self.price = price
@@ -7,5 +8,35 @@ class Menu:
         sale = self.price * (1 - percent /100)
         print(f"{self.name} 할인가: {sale}원")
 
-coffee = Menu("아메리카노", 4000)
-coffee.discount(15)
+class Employee:
+    def __init__(self, name, department, salary):
+        self.name = name
+        self.department = department
+        self.salary = salary
+
+    def increase(self, percent):
+        sal = self.salary * (1 + percent/100)
+        print(f"{self.name}님의 연봉은 {sal}만원 으로 인상되셧습니다")
+
+class coupang:
+    def __init__(self, name, num):
+        self.name = name
+        self.num = num
+    
+    def add_stock(self, amount):
+        self.num = self.num + amount
+        print(f"{self.num}개 추가되었습니다.")
+
+    def remove_stock(self, amount):
+        self.num = self.num - amount
+        print(f"{self.num}개 사라짐")
+        
+item = coupang("신발", 100)
+item.add_stock(50)
+item.remove_stock(30)
+
+
+# humen = Employee("근원","개발부서",3000)
+# humen.increase(12)
+# coffee = Menu("아메리카노", 4000)
+# coffee.discount(15)
